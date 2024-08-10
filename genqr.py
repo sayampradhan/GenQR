3aa6b9c4cd0149d5ad0eabc036ebd224cea5b6da
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2024-08-10 23:38:12 +0530

    Update genqr.py

--- genqr.py
+++ genqr.py
@@ -22,7 +22,7 @@ from io import BytesIO
 
 st.set_page_config(
     page_title="GenQR",
-    page_icon="𖦹",
+    page_icon="🌌",
 )
 
 def get_image_download_link(img, filename):
@@ -30,7 +30,7 @@ def get_image_download_link(img, filename):
     img.save(buffered, format="JPEG")
     return buffered.getvalue()
 
-st.title(':violet[Gen]:rainbow[QR] 🌌𖦹')
+st.title(':violet[Gen]:rainbow[QR] 🌌')
 
 image_placeholder = st.empty()
 download_button_placeholder = st.empty()
